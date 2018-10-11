c5407c16386d0816ca142bf37e77064bbd21607f
brahim <brahhim.mess@gmail.com>
2018-10-11 14:03:15 +0200

    jwt put in place

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -1,117 +1,94 @@
 <?php
+
 namespace AppBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+    use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
-* @ORM\Entity
-* @UniqueEntity(fields="email", message="Email already taken")
-* @UniqueEntity(fields="username", message="Username already taken")
-*/
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
+ */
 class User implements UserInterface
 {
-        /**
-        * @ORM\Id
-        * @ORM\Column(type="integer")
-        * @ORM\GeneratedValue(strategy="AUTO")
-        */
-        private $id;
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
 
-        /**
-        * @ORM\Column(type="string", length=255, unique=true)
-        * @Assert\NotBlank()
-        * @Assert\Email()
-        */
-        private $email;
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 
-        /**
-        * @ORM\Column(type="string", length=255, unique=true)
-        * @Assert\NotBlank()
-        */
-        private $username;
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 
-        /**
-        * @Assert\NotBlank()
-        * @Assert\Length(max=4096)
-        */
-        private $plainPassword;
+    /**
+     * @return mixed
+     */
+    public function getisActive()
+    {
+        return $this->isActive;
+    }
 
-        /**
-        * The below length depends on the "algorithm" you use for encoding
-        * the password, but this works well with bcrypt.
-        *
-        * @ORM\Column(type="string", length=64)
-        */
-        private $password;
-
-        /**
-        * @ORM\Column(type="array")
-        */
-        private $roles;
-
-        public function __construct()
-        {
-        $this->roles = array('ROLE_USER');
-        }
-
-        // other properties and methods
-
-        public function getEmail()
-        {
-        return $this->email;
-        }
-
-        public function setEmail($email)
-        {
-        $this->email = $email;
-        }
-
-        public function getUsername()
-        {
+    /**
+     * @param mixed $isActive
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+    }
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    private $username;
+    /**
+     * @ORM\Column(type="string", length=500)
+     */
+    private $password;
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+    public function __construct($username)
+    {
+        $this->isActive = true;
+        $this->username = $username;
+    }
+    public function getUsername()
+    {
         return $this->username;
-        }
-
-        public function setUsername($username)
-            {
-            $this->username = $username;
-            }
-
-        public function getPlainPassword()
-        {
-        return $this->plainPassword;
-        }
-
-        public function setPlainPassword($password)
-        {
-        $this->plainPassword = $password;
-        }
-
-        public function getPassword()
-        {
-        return $this->password;
-        }
-
-        public function setPassword($password)
-        {
-        $this->password = $password;
-        }
-
-        public function getSalt()
-        {
-        // The bcrypt and argon2i algorithms don't require a separate salt.
-        // You *may* need a real salt if you choose a different encoder.
+    }
+    public function getSalt()
+    {
         return null;
-        }
-
-        public function getRoles()
-        {
-        return $this->roles;
-        }
-
-        public function eraseCredentials()
-        {
-        }
+    }
+    public function getPassword()
+    {
+        return $this->password;
+    }
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+    public function eraseCredentials()
+    {
+    }
 }
